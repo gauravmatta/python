ccb3a7b3b1c3636180814035082f5428fde40ef2
Gaurav Matta <gaurav.matta@gmail.com>
2019-04-17 12:42:10 +0530

    GAN Example added
    Signed-off-by: Gaurav Matta <gaurav.matta@gmail.com>

---
+++ flask_apps/hello_app.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import request
+from flask import jsonify
+from flask import Flask
+
+app=Flask(__name__)
+@app.route('/hello',methods=['POST'])
+def hello():
+    message = request.get_json(force=True)
+    name=message['name']
+    response={
+        'greeting':'Hello, '+ name + '!'
+    }
+    return jsonify(response)
+
---
+++ flask_apps/sample_app.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+app=Flask(__name__)
+@app.route('/sample')
+def running():
+    return 'Flask is running!'
+
