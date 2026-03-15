d2e0ef2dbb774b99cf0d9ca2742a7bc91339b252
gauravmatta <gaurav.matta.tech@gmail.com>
2026-03-16 02:25:40 +0530

    updated dags

--- airflow/dags/hello_world.py
+++ airflow/dags/hello_world.py
@@ -6,6 +6,5 @@ from airflow.providers.standard.operators.python import PythonOperator
 
 def hello_world():
   print("Hello, World!")
-with DAG('hello_world', start_date=datetime(2022, 1, 1), schedule='@daily') as dag:
+with DAG('hello_world', start_date=datetime(2026, 1, 1), schedule='@daily') as dag:
   task = PythonOperator(task_id='hello_task', python_callable=hello_world)
-  task
