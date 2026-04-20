fc16b17fcf507d75fd9d23b02409f8470b992cf5
gauravmatta <gaurav.matta.tech@gmail.com>
2026-04-20 22:11:09 +0530

    added list

--- basic/src/datatypes/list_mutable.py
+++ basic/src/datatypes/list_mutable.py
@@ -1,4 +1,10 @@
 ingredients=["water","milk","black tea"]
 ingredients.append("sugar")
 print(f"Ingredients: {ingredients}")
-ingredients.append("")
+ingredients.remove("milk")
+print(f"Ingredients: {ingredients}")
+
+spice_options=["ginger","cardamom"]
+tea_ingredients=["water","milk","tea leaves"]
+tea_ingredients.extend(ingredients)
+print(f"Tea Ingredients: {tea_ingredients}")
