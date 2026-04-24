406c9fea333e9a281b745762be2b7964d5d8c5ca
gauravmatta <gaurav.matta.tech@gmail.com>
2026-04-24 23:02:05 +0530

    added if condition

---
+++ basic/src/conditionals/if_condition.py
@@ -0,0 +1,6 @@
+kettle_boiled=True
+if kettle_boiled:
+    print(f"Boiled Kettle: {kettle_boiled}")
+kettle_boiled_done=False
+if kettle_boiled_done:
+    print(f"Boiled Kettle Done: {kettle_boiled_done}")
