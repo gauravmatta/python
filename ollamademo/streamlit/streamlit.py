bde1df7735f2e250d7c959ac036bb7d989bd224b
gauravmatta <gaurav.matta.tech@gmail.com>
2026-06-28 12:42:53 +0530

    added streamlit

--- ollamademo/streamlit/streamlit.py
+++ ollamademo/streamlit/streamlit.py
@@ -1,3 +1,5 @@
+import ollama
+
 import streamlit as st
 
 st.title("Ollama!")
@@ -5,3 +7,5 @@ prompt = st.text_area("Enter your prompt here:")
 if st.button("Okay"):
     if prompt:
         st.markdown(prompt)
+        response = ollama.generate('gemma3', prompt=prompt)
+        st.markdown(response['response'])
